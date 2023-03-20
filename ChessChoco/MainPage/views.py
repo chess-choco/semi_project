a63036d91d9375ed99ecc3e4e08ae48df8030be3
Amras <amrasanor@naver.com>
2023-03-20 14:24:09 +0900

    added_rules.html

--- ChessChoco/MainPage/views.py
+++ ChessChoco/MainPage/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("MainPage test")
+    return render(request, 'MainPage/main.html')
