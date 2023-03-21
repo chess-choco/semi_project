a50051bff71e7380e7e5382826d3f381cf81e808
hoopmad <hoopmad22@gmail.com>
2023-03-21 14:56:37 +0900

    updated opening page and .gitignore

--- ChessChoco/OpeningAnalysis/models.py
+++ ChessChoco/OpeningAnalysis/models.py
@@ -4,3 +4,4 @@ class ChessOpening(models.Model):
     name = models.CharField(max_length=200)
     moves = models.TextField(null=True)
     description = models.TextField()
+    variation = models.TextField(default='없음')
--- ChessChoco/OpeningAnalysis/views.py
+++ ChessChoco/OpeningAnalysis/views.py
@@ -7,12 +7,5 @@ def index(request):
     return render(request, "opening.html")
 
 def chess_openings(request):
-    openings = [
-        {'name': 'Sicilian Defense', 'moves': '1.e4 c5'},
-        {'name': 'French Defense', 'moves': '1.e4 e6'},
-        {'name': 'Caro-Kann Defense', 'moves': '1.e4 c6'},
-        {'name': 'Ruy Lopez', 'moves': '1.e4 e5 2.Nf3 Nc6 3.Bb5'},
-        {'name': 'Italian Game', 'moves': '1.e4 e5 2.Nf3 Nc6 3.Bc4'},
-    ]
-    context = {'openings': openings}
-    return render(request, 'chess_opening.html', context)
+    openings = ChessOpening.objects.all()
+    return render(request, 'chess_opening.html', {'openings': openings})
