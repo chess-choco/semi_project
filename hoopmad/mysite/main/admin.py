3490b2eb1d591de749bf5f9a0ce175611878a28d
hoopmad <hoopmad22@gmail.com>
2023-04-04 10:32:56 +0900

    deleted unused file

--- hoopmad/mysite/main/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from main.models import ChessPiece, ChessOpening, ChessResult, ChessBoard
-
-admin.site.register(ChessPiece)
-admin.site.register(ChessOpening)
-admin.site.register(ChessResult)
-admin.site.register(ChessBoard)
